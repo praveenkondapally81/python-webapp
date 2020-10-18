1a6d60cc857d5b0bb6d8d26a3a69c44e1fa5282c
Praveen <Praveenkondapally81@gmail.com>
2020-10-18 10:10:37 -0700

    added 1 line

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/how are you doing')
 def hello():
     return 'I am doing good, how about you?'
 
